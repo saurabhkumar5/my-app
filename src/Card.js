a0f7e632b9e441117baf41afa5d61ea7f4b762f8
saurabh kumar <saurabhkumar9493@gmail.com>
2024-01-18 18:49:03 +0530

    props

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Card.css'
-function Card({name,rating,para}) {
+function Card({name,rating,para,img}) {
   return (
    
     <div className="card">
       
-      <img  src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/f596863116da4b4688b48e813e25000d" />
+      <img  src={img} />
       <h2>{name}</h2>
       <h3>{rating} stars</h3>
       <p>{para}</p>
